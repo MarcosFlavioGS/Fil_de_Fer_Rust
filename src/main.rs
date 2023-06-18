dbea39919a8bbc4deace6593e9c632d8eae3bfc4
Marcos Flavio <mfghost69@gmail.com>
2023-06-18 00:56:03 -0300

    Refact (main): Changing functions to Enum Implementations for init map and isometric.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ extern crate sdl2;
 
 mod put_pixel;
 mod map_reader;
-mod init_map;
-mod isometric;
 
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -13,13 +11,61 @@ use std::time::Duration;
 
 use crate::put_pixel::put_pixel;
 use crate::map_reader::map_reader;
-use crate::init_map::init_map;
-use crate::isometric::isometric;
 
 pub struct PointStruct {
     point: Point,
     color: Color,
     value: u32,
+}
+
+pub enum Map {
+    Map(Vec<Vec<PointStruct>>),
+}
+
+impl Map {
+    fn init_map(self, matrix: &Vec<Vec<u32>>) -> Map {
+        match self {
+            Map::Map(mut map) => {
+                let mut x = 5;
+                let mut y = 5;
+                for line in matrix.iter() {
+                    let mut tmp: Vec<PointStruct> = Vec::new();
+                    for value in line.iter() {
+                        let point = PointStruct {
+                            point: Point::new(x, y),
+                            color: Color::RGB(255, 255, 255),
+                            value: *value,
+                        };
+                        tmp.push(point);
+                        x += 10;
+                    }
+                    x = 5;
+                    y += 10;
+                    map.push(tmp);
+                }
+                Map::Map(map)
+            }
+        }
+    }
+}
+
+impl Map {
+    fn isometric(self) ->Vec<Vec<PointStruct>> {
+        match self {
+            Map::Map(mut map) => {
+                for line in map.iter_mut() {
+                    for point in line.iter_mut() {
+                        let x = point.point.x;
+                        point.point.x = (x - point.point.y) * 2;
+                        point.point.y = (x + point.point.y) / 2 - point.value as i32;
+                        point.point.x += 600;
+                        point.point.y += 300;
+                    }
+                }
+                map
+            }
+        }
+    }
 }
 
 fn main() {
@@ -32,14 +78,14 @@ fn main() {
         .unwrap();
     let mut canvas = window.into_canvas().build().unwrap();
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut map: Vec<Vec<PointStruct>> = Vec::new();
+    let map = Map::Map(Vec::new());
+    let isometric_map: Vec<Vec<PointStruct>>;
 
     if let Ok(matrix) = map_reader("maps/42.fdf") {
-        init_map(&mut map, &matrix);
+        isometric_map = map.init_map(&matrix).isometric();
     } else {
         panic!("File not found !");
     }
-    isometric(&mut map);
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     'running: loop {
         canvas.clear();
@@ -53,7 +99,7 @@ fn main() {
                 _ => {}
             }
         }
-        for line in &map {
+        for line in &isometric_map {
             for point in line {
                 put_pixel(&mut canvas, point.point, point.color);
             }
